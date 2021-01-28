70337a389ab2f473cf25717ab4aea7cc897bc921
root <qiuqiangkong@gmail.com>
2021-01-28 15:49:28 +0000

    from torchlibrosa.stft import ...

--- torchlibrosa/__init__.py
+++ torchlibrosa/__init__.py
@@ -1,2 +1,6 @@
-import torchlibrosa.stft
 import torchlibrosa.augmentation
+import torchlibrosa.stft
+
+from torchlibrosa.augmentation import DropStripes, SpecAugmentation
+from torchlibrosa.stft import DFTBase, DFT, STFT, ISTFT, Spectrogram, \
+	LogmelFilterBank, Enframe, Scalar

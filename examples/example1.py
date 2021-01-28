833437d6394401ce4caf0082f325e7067143d6cb
root <qiuqiangkong@gmail.com>
2021-01-28 15:46:22 +0000

    tl.stft.STFT -> tl.STFT, etc

--- examples/example1.py
+++ examples/example1.py
@@ -1,22 +1,27 @@
 import torch
 import torchlibrosa as tl
 
-# Data
-x = torch.zeros(1, 32000)	# (batch_size, samples_num)
+batch_size = 16
+sample_rate = 22050
+win_length = 2048
+hop_length = 512
+n_mels = 128
+
+batch_audio = torch.empty(batch_size, sample_rate).uniform_(-1, 1)  # (batch_size, sample_rate)
 
 # Spectrogram
-spectrogram_extractor = tl.stft.Spectrogram(n_fft=1024, hop_length=250)
-sp = spectrogram_extractor.forward(x)	# (batch_size, 1, time_steps, freq_bins)
+spectrogram_extractor = tl.Spectrogram(n_fft=win_length, hop_length=hop_length)
+sp = spectrogram_extractor.forward(batch_audio)   # (batch_size, 1, time_steps, freq_bins)
 
 # Log mel spectrogram
-logmel_extractor = tl.stft.LogmelFilterBank(sr=32000, n_fft=1024, n_mels=64)
-logmel = logmel_extractor.forward(sp)	# (batch_size, 1, time_steps, freq_bins)
+logmel_extractor = tl.LogmelFilterBank(sr=sample_rate, n_fft=win_length, n_mels=n_mels)
+logmel = logmel_extractor.forward(sp)   # (batch_size, 1, time_steps, mel_bins)
 
 # STFT
-stft_extractor = tl.stft.STFT(n_fft=1024, hop_length=250)
-(real, imag) = stft_extractor.forward(x)
-"""real: (batch_size, 1, time_steps, freq_bins), imag: (batch_size, 1, time_steps, freq_bins)"""
+stft_extractor = tl.STFT(n_fft=win_length, hop_length=hop_length)
+(real, imag) = stft_extractor.forward(batch_audio)
+# real: (batch_size, 1, time_steps, freq_bins), imag: (batch_size, 1, time_steps, freq_bins) #
 
 # ISTFT
-istft_extractor = tl.stft.ISTFT(n_fft=1024, hop_length=250)
-y = istft_extractor.forward(real, imag, x.shape[-1])	# (batch_size, samples_num)
+istft_extractor = tl.ISTFT(n_fft=win_length, hop_length=hop_length)
+y = istft_extractor.forward(real, imag, length=batch_audio.shape[-1])    # (batch_size, samples_num)

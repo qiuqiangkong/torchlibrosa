66b388b4027de7ef0d780921ed779fb8e862419a
unknown <abcdmoto0509@outlook.jp>
2022-05-20 09:17:02 +0900

    add fill_value argument

--- torchlibrosa/augmentation.py
+++ torchlibrosa/augmentation.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 
 class DropStripes(nn.Module):
-    def __init__(self, dim, drop_width, stripes_num):
+    def __init__(self, dim, drop_width, stripes_num, fill_value):
         """Drop stripes. 
 
         Args:
@@ -21,6 +21,7 @@ class DropStripes(nn.Module):
         self.dim = dim
         self.drop_width = drop_width
         self.stripes_num = stripes_num
+        self.fill_value = fill_value
 
     def forward(self, input):
         """input: (batch_size, channels, time_steps, freq_bins)"""
@@ -48,14 +49,14 @@ class DropStripes(nn.Module):
             bgn = torch.randint(low=0, high=total_width - distance, size=(1,))[0]
 
             if self.dim == 2:
-                e[:, bgn : bgn + distance, :] = 0
+                e[:, bgn : bgn + distance, :] = self.fill_value
             elif self.dim == 3:
-                e[:, :, bgn : bgn + distance] = 0
+                e[:, :, bgn : bgn + distance] = self.fill_value
 
 
 class SpecAugmentation(nn.Module):
     def __init__(self, time_drop_width, time_stripes_num, freq_drop_width, 
-        freq_stripes_num):
+        freq_stripes_num, fill_value=0):
         """Spec augmetation. 
         [ref] Park, D.S., Chan, W., Zhang, Y., Chiu, C.C., Zoph, B., Cubuk, E.D. 
         and Le, Q.V., 2019. Specaugment: A simple data augmentation method 
@@ -71,10 +72,10 @@ class SpecAugmentation(nn.Module):
         super(SpecAugmentation, self).__init__()
 
         self.time_dropper = DropStripes(dim=2, drop_width=time_drop_width, 
-            stripes_num=time_stripes_num)
+            stripes_num=time_stripes_num, fill_value=fill_value)
 
         self.freq_dropper = DropStripes(dim=3, drop_width=freq_drop_width, 
-            stripes_num=freq_stripes_num)
+            stripes_num=freq_stripes_num, fill_value=fill_value)
 
     def forward(self, input):
         x = self.time_dropper(input)
@@ -90,7 +91,7 @@ if __name__ == '__main__':
     pt_data = torch.Tensor(np_data)
 
     spec_augmenter = SpecAugmentation(time_drop_width=64, time_stripes_num=2, 
-        freq_drop_width=16, freq_stripes_num=2)
+        freq_drop_width=16, freq_stripes_num=2, fill_value=0)
 
     # Training stage
     spec_augmenter.train()    # set to spec_augmenter.eval() for evaluation
